738ae462e8d95319a9bda535eca42f5a65970a18
s70160 <67191334+s70160@users.noreply.github.com>
2020-12-09 02:08:27 +0800

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -1,14 +1,50 @@
 from cereal import car
 from common.realtime import DT_CTRL
 from selfdrive.car import apply_std_steer_torque_limits
-from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, create_lfa_mfa
-from selfdrive.car.hyundai.values import Buttons, SteerLimitParams, CAR
+from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, create_lfa_mfa, \
+                                             create_scc11, create_scc12, create_scc13, create_scc14, \
+                                             create_scc42a, create_scc7d0, create_fca11, create_fca12, create_mdps12
+from selfdrive.car.hyundai.values import Buttons, SteerLimitParams, CAR, FEATURES
 from opendbc.can.packer import CANPacker
 from common.dp_common import common_controller_ctrl
 from common.params import Params
+from selfdrive.config import Conversions as CV
+from selfdrive.controls.lib.longcontrol import LongCtrlState
+from selfdrive.car.hyundai.carstate import GearShifter
 
 VisualAlert = car.CarControl.HUDControl.VisualAlert
 
+# Accel Hard limits
+ACCEL_HYST_GAP = 0.1  # don't change accel command for small oscillations within this value
+ACCEL_MAX = 3.5  # 2.0 m/s2
+ACCEL_MIN = -4.5  # 3.5   m/s2
+ACCEL_SCALE = 1.
+
+def accel_hysteresis(accel, accel_steady):
+
+# for small accel oscillations within ACCEL_HYST_GAP, don't change the accel command
+  if accel > accel_steady + ACCEL_HYST_GAP:
+    accel_steady = accel - ACCEL_HYST_GAP
+  elif accel < accel_steady - ACCEL_HYST_GAP:
+    accel_steady = accel + ACCEL_HYST_GAP
+  accel = accel_steady
+  
+  return accel, accel_steady
+
+def accel_rate_limit(accel_lim, prev_accel_lim):
+    
+  if accel_lim > 0:
+    if accel_lim > prev_accel_lim:
+      accel_lim = min(accel_lim, prev_accel_lim + 0.02)
+    else:
+      accel_lim = max(accel_lim, prev_accel_lim - 0.035)
+  else:
+    if accel_lim < prev_accel_lim:
+      accel_lim = max(accel_lim, prev_accel_lim - 0.035)
+    else:
+      accel_lim = min(accel_lim, prev_accel_lim + 0.01)
+
+  return accel_lim
 
 def process_hud_alert(enabled, fingerprint, visual_alert, left_lane,
                       right_lane, left_lane_depart, right_lane_depart):
@@ -43,7 +79,35 @@ class CarController():
     self.car_fingerprint = CP.carFingerprint
     self.steer_rate_limited = False
     self.last_resume_frame = 0
-
+    
+    # hkg
+    self.cp_oplongcontrol = CP.openpilotLongitudinalControl
+    self.packer = CANPacker(dbc_name)
+    self.accel_steady = 0
+    self.accel_lim_prev = 0.
+    self.accel_lim = 0.
+    self.steer_rate_limited = False
+    self.p = SteerLimitParams(CP)
+    self.usestockscc = True
+    self.lead_visible = False
+    self.lead_debounce = 0
+    self.gapsettingdance = 2
+    self.gapcount = 0
+    self.current_veh_speed = 0
+    self.lfainFingerprint = CP.lfaAvailable
+    self.vdiff = 0
+    self.resumebuttoncnt = 0
+    self.lastresumeframe = 0
+    self.fca11supcnt = self.fca11inc = self.fca11alivecnt = self.fca11cnt13 = self.scc11cnt = self.scc12cnt = 0
+    self.counter_init = False
+    self.fca11maxcnt = 0xD
+    self.radarDisableActivated = False
+    self.radarDisableResetTimer = 0
+    self.radarDisableOverlapTimer = 0
+    self.sendaccmode = not CP.radarDisablePossible
+    self.enabled = False
+    self.sm = messaging.SubMaster(['controlsState'])
+    
     # dp
     self.last_blinker_on = False
     self.blinker_end_frame = 0.
@@ -51,6 +115,7 @@ class CarController():
 
   def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, visual_alert,
              left_lane, right_lane, left_lane_depart, right_lane_depart, dragonconf):
+    -------------------------------------not yet-------------------------------------
     # Steering Torque
     new_steer = actuators.steer * self.p.STEER_MAX
     apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.p)
